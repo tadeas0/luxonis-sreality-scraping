9764eacb7f3243aa2e4820914bf767817fb1cc61
Tadeas Pala <tadapala@seznam.cz>
2023-10-17 16:23:36 +0200

    implement scraper scheduling

---
+++ scraper/schedule_scraper.py
@@ -0,0 +1,15 @@
+import schedule
+import time
+from subprocess import call
+
+
+def run_spider():
+    call(['scrapy', 'crawl', 'sreality'])
+
+
+if __name__ == "__main__":
+    schedule.every(5).minutes.do(run_spider)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
