2d9b74fe6a1427e8c5741cc06f043cd6c141e550
Tadeas Pala <tadapala@seznam.cz>
2023-10-19 16:24:30 +0200

    separate app server initialization and routes

---
+++ server/estates_bp.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, render_template
+from db_helpers import get_db
+from settings import PAGE_SIZE
+
+
+estates_bp = Blueprint("auth", __name__)
+
+
+@estates_bp.route("/", defaults={"page_number": 1})
+@estates_bp.route("/page/<int:page_number>")
+def render_page(page_number: int):
+    try:
+        db = get_db()
+        estates = db.get_estates(
+            take=PAGE_SIZE,
+            skip=(page_number - 1) * PAGE_SIZE
+        )
+        count = db.get_estate_count()
+        max_page = ((count - 1) // PAGE_SIZE) + 1
+
+        page_range = 2
+        lo = max(page_number - page_range, 1)
+        hi = min(page_number + page_range, max_page)
+        page_list: list[int | None] = list(range(lo, hi + 1))
+        if hi != max_page:
+            page_list += [None, max_page]
+        if lo > 1:
+            page_list.insert(0, None)
+            page_list.insert(0, 1)
+
+        return render_template(
+            "index.jinja",
+            estates=estates,
+            total_estates=count,
+            page_list=page_list,
+            curr_page=page_number
+        )
+    except Exception:
+        return render_template(
+            "index.jinja",
+            estates=[],
+            total_estates=0,
+            page_list=[],
+            curr_page=page_number
+        )
