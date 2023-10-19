cb1d950266b295d3f822fe275fcd0d248f3da09d
Tadeas Pala <tadapala@seznam.cz>
2023-10-19 17:30:59 +0200

    add logging to server

--- server/server.py
+++ server/server.py
@@ -1,7 +1,25 @@
 from flask import Flask
+import logging
+import settings
+
+
+def setup_logging():
+    logger = logging.getLogger(settings.DEFAULT_LOGGER)
+
+    logger.setLevel(logging.INFO)
+
+    sh = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+    return logger
 
 
 def create_app():
+    logger = setup_logging()
+
     app = Flask(__name__)
 
     from db_helpers import init_app
@@ -9,6 +27,8 @@ def create_app():
 
     from estates_bp import estates_bp
     app.register_blueprint(estates_bp)
+
+    logger.info("Application initialized")
 
     return app
 
--- server/settings.py
+++ server/settings.py
@@ -1,6 +1,7 @@
 import os
 
 POSTGRES_URL = os.environ.get("POSTGRES_URL")
+DEFAULT_LOGGER = "server"
 
 # Number of results per page
 PAGE_SIZE = 5
